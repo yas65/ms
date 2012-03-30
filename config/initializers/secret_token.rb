c759f72e513247191c788970f48605f2926c4f03
Kohei Yasumura <yasumura@f-plat.com>
2012-03-30 20:42:24 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieShare::Application.config.secret_token = '744dd1a4274b7ca7926b5bf1fc495e74c81de2ee9c47c40d53e728662e99a060134485d8495302772dcf10f30f9d8f172257c18b228e18785f5b3c7b834a5220'
