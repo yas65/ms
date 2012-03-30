355f868f9f46e32aebbfdc36bda88e04a5ba5cff
Kohei Yasumura <yasumura@f-plat.com>
2012-03-30 21:32:51 +0900

    Generate some files for start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MovieShare::Application.routes.draw do
+  root :to => "sessions#new"
+  resources :sessions
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
